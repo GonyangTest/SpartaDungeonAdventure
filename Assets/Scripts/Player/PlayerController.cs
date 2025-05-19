63fd8e4070b81b79c13dc27af0e2f914b0acdf41
GonyangTest <112749400+GonyangTest@users.noreply.github.com>
2025-05-19 21:29:49 +0900

    feat: 플레이어 컨트롤러 구현 및 입력 시스템 추가 - PlayerController.cs에 이동 및 점프 기능 추가 - Unity Input System을 사용하여 입력 처리 - 테스트 환경 맵 추가 - .gitignore에 Externals 폴더 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private int maxHealth;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int maxHealth = 100;
     private int currentHealth;
     private Rigidbody rb;
+
+    public LayerMask groundLayer;
+
+    private Vector2 _moveInput;
 
     public event Action<int, int> OnHealthChanged;
 
@@ -17,8 +22,64 @@ public class PlayerController : MonoBehaviour
         currentHealth = maxHealth;
     }
 
-    private void Move() { }
-    private void Jump() { }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 moveDirection = transform.forward * _moveInput.y + transform.right * _moveInput.x;
+        moveDirection *= moveSpeed;
+        moveDirection.y = rb.velocity.y;
+        rb.velocity = moveDirection;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            _moveInput = Vector2.zero;
+        }
+    }
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private bool isGrounded()
+    {
+
+        Ray[] rays = new Ray[4];
+        rays[0] = new Ray(transform.position + (transform.forward * 0.2f) + transform.up * 0.01f, Vector3.down); // 앞쪽 첫번째 레이어
+        rays[1] = new Ray(transform.position + (-transform.forward * 0.2f) + transform.up * 0.01f, Vector3.down); // 앞쪽 두번째 레이어
+        rays[2] = new Ray(transform.position + (transform.right * 0.2f) + transform.up * 0.01f, Vector3.down); // 오른쪽 첫번째 레이어
+        rays[3] = new Ray(transform.position + (-transform.right * 0.2f) + transform.up * 0.01f, Vector3.down); // 왼쪽 첫번째 레이어
+
+        foreach (Ray ray in rays)
+        {
+            Debug.Log(ray.origin);
+            Debug.DrawRay(ray.origin, ray.direction * 0.1f, Color.red);
+            if (Physics.Raycast(ray, 0.1f, groundLayer))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void TakeDamage(int damage) { }
     public void Heal(int amount) { }
     public void ApplyForce(Vector3 force, ForceMode mode) { }
